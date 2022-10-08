65c2515315a7ede139320972d41d14b91f1c4e11
linhaojun <lhj13401792631@163.com>
2022-10-08 18:16:56 +0800

    :art: 优化后端listener代码

--- aurora-springboot/src/main/java/com/aurora/listener/AuroraListener.java
+++ aurora-springboot/src/main/java/com/aurora/listener/AuroraListener.java
@@ -1,22 +1,35 @@
 package com.aurora.listener;
 
 import com.aurora.entity.ExceptionLog;
+import com.aurora.entity.OperationLog;
 import com.aurora.event.ExceptionLogEvent;
+import com.aurora.event.OperationLogEvent;
 import com.aurora.mapper.ExceptionLogMapper;
+import com.aurora.mapper.OperationLogMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ExceptionLogListener {
+public class AuroraListener {
+
+    @Autowired
+    private OperationLogMapper operationLogMapper;
 
     @Autowired
     private ExceptionLogMapper exceptionLogMapper;
+
+    @Async
+    @EventListener(OperationLogEvent.class)
+    public void saveOperationLog(OperationLogEvent operationLogEvent) {
+        operationLogMapper.insert((OperationLog) operationLogEvent.getSource());
+    }
 
     @Async
     @EventListener(ExceptionLogEvent.class)
     public void saveExceptionLog(ExceptionLogEvent exceptionLogEvent) {
         exceptionLogMapper.insert((ExceptionLog) exceptionLogEvent.getSource());
     }
+
 }
