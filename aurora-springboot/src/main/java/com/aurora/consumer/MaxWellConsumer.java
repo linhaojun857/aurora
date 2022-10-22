46aaac5c8916eae0bbde48b1dac1bb9d636b535e
linhaojun <lhj13401792631@163.com>
2022-10-22 20:35:59 +0800

    :art: 优化代码

--- aurora-springboot/src/main/java/com/aurora/consumer/MaxWellConsumer.java
+++ aurora-springboot/src/main/java/com/aurora/consumer/MaxWellConsumer.java
@@ -13,32 +13,23 @@ import org.springframework.stereotype.Component;
 
 import static com.aurora.constant.RabbitMQConstant.MAXWELL_QUEUE;
 
-
-/**
- * @author 花未眠
- * maxwell监听数据
- */
 @Component
 @RabbitListener(queues = MAXWELL_QUEUE)
 public class MaxWellConsumer {
+
     @Autowired
     private ElasticsearchMapper elasticsearchMapper;
 
     @RabbitHandler
     public void process(byte[] data) {
-        // 获取监听信息
         MaxwellDataDTO maxwellDataDTO = JSON.parseObject(new String(data), MaxwellDataDTO.class);
-        // 获取文章数据
         Article article = JSON.parseObject(JSON.toJSONString(maxwellDataDTO.getData()), Article.class);
-        // 判断操作类型
         switch (maxwellDataDTO.getType()) {
             case "insert":
             case "update":
-                // 更新es文章
                 elasticsearchMapper.save(BeanCopyUtil.copyObject(article, ArticleSearchDTO.class));
                 break;
             case "delete":
-                // 删除文章
                 elasticsearchMapper.deleteById(article.getId());
                 break;
             default:
--- aurora-springboot/src/main/java/com/aurora/consumer/SubscribeConsumer.java
+++ aurora-springboot/src/main/java/com/aurora/consumer/SubscribeConsumer.java
@@ -25,9 +25,6 @@ import static com.aurora.constant.RabbitMQConstant.SUBSCRIBE_QUEUE;
 @RabbitListener(queues = SUBSCRIBE_QUEUE)
 public class SubscribeConsumer {
 
-    /**
-     * 邮箱号
-     */
     @Value("${spring.mail.username}")
     private String email;
 
@@ -57,7 +54,7 @@ public class SubscribeConsumer {
             if (article.getUpdateTime() == null) {
                 message.setText("花未眠的个人博客发布了新的文章，地址：" + websiteUrl + "/articles/" + articleId);
             } else {
-//                message.setText("花未眠的个人博客对《" + article.getArticleTitle() + "》进行了更新，地址：" + websiteUrl + "/articles/" + articleId);
+                message.setText("花未眠的个人博客对《" + article.getArticleTitle() + "》进行了更新，地址：" + websiteUrl + "/articles/" + articleId);
             }
             javaMailSender.send(message);
         }
