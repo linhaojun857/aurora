dcd46cdb209c1e4b266c07254c723ae1cce60433
zhengjh <2232425497@qq.com>
2022-12-02 16:35:09 +0800

    --n 字符串修改常量；测试提交

--- aurora-springboot/src/main/java/com/aurora/handler/AuthenticationFailHandlerImpl.java
+++ aurora-springboot/src/main/java/com/aurora/handler/AuthenticationFailHandlerImpl.java
@@ -1,6 +1,7 @@
 package com.aurora.handler;
 
 import com.alibaba.fastjson.JSON;
+import com.aurora.constant.CommonConstant;
 import com.aurora.model.vo.ResultVO;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -18,7 +19,7 @@ public class AuthenticationFailHandlerImpl implements AuthenticationFailureHandl
 
     @Override
     public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
-        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        httpServletResponse.setContentType(CommonConstant.APPLICATION_JSON);
         httpServletResponse.getWriter().write(JSON.toJSONString(ResultVO.fail(e.getMessage())));
     }
 
