8f53aa3d19adec2fb7ea2b9980ed1caaed0c8ed8
linhaojun <lhj13401792631@163.com>
2023-01-25 19:48:41 +0800

    :art: 优化代码

--- aurora-vue/aurora-blog/src/components/Footer.vue
+++ aurora-vue/aurora-blog/src/components/Footer.vue
@@ -5,12 +5,12 @@
         class="bg-ob-deep-900 rounded-lg max-w-10/12 lg:max-w-screen-2xl text-sm text-ob-normal w-full py-6 px-6 grid grid-rows-1 lg:grid-rows-none lg:grid-cols-4 justify-center items-center gap-8 h-36 mx-auto">
         <div
           class="flex flex-col lg:flex-row gap-6 lg:gap-12 row-span-1 lg:col-span-5 text-center lg:text-left mx-auto">
-          <ul class="flex flex-col gap-1.5 mx-auto">
+          <ul class="flex flex-col gap-2 mx-auto">
             <li>
-              Copyright © 2019 - {{ currentYear }}
+              Copyright © 2022 - {{ currentYear }}
               <b class="font-extrabold">{{ websiteConfig.author }}</b>
             </li>
-            <li v-if="websiteConfig.beian_number != ''" class="flex flex-row gap-3 mx-auto">
+            <li v-if="websiteConfig.beian_number != ''" class="flex flex-row mx-auto">
               <a href="https://beian.miit.gov.cn/" target="_blank">
                 <b class="font-extrabold border-b-2 border-ob hover:text-ob"> {{ websiteConfig.beianNumber }} </b>
               </a>
--- aurora-vue/aurora-blog/src/utils/markdown.ts
+++ aurora-vue/aurora-blog/src/utils/markdown.ts
@@ -1,5 +1,9 @@
 export default function markdownToHtml(content: any) {
   const MarkdownIt = require('markdown-it')
-  const md = new MarkdownIt().use(require('markdown-it-katex-external')).use(require('markdown-it-emoji'))
+  const md = new MarkdownIt({
+    html: true
+  })
+    .use(require('markdown-it-katex-external'))
+    .use(require('markdown-it-emoji'))
   return md.render(content)
 }
