dd79ca09589a4f154e1fd35e6e3a81b72c88e2dd
linhaojun <lhj13401792631@163.com>
2023-02-13 23:05:33 +0800

    :art: 优化代码

--- aurora-vue/aurora-blog/src/api/api.ts
+++ aurora-vue/aurora-blog/src/api/api.ts
@@ -44,7 +44,7 @@ export default {
     return axios.get('/api/articles/' + articleId)
   },
   getAllCategories: () => {
-    return axios.get('/api//categories/all')
+    return axios.get('/api/categories/all')
   },
   getAllTags: () => {
     return axios.get('/api/tags/all')
