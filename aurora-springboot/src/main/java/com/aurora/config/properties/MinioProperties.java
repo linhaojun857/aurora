62a20ce11ab3910c62dcc1baa3d3f6e0af0a526e
linhaojun <lhj13401792631@163.com>
2023-06-25 20:46:37 +0800

    修改注解

--- aurora-springboot/src/main/java/com/aurora/config/properties/MinioProperties.java
+++ aurora-springboot/src/main/java/com/aurora/config/properties/MinioProperties.java
@@ -2,7 +2,7 @@ package com.aurora.config.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
